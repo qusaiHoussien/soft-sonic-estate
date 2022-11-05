9f8fe4e95c98dc380005989035d7293d4f0bf37a
qusaiHoussien <111251532+qusaiHoussien@users.noreply.github.com>
2022-11-05 21:32:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './customBootsrtap.scss';
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
