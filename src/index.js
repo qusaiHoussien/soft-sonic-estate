9ec86cfea396c265c3a3ba52b3c54e14cb8933a2
qusaiHoussien <111251532+qusaiHoussien@users.noreply.github.com>
2022-11-05 22:24:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import './customBootsrtap.scss';
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
