9ff4528826ac07b98dd4af8d941a6379aa26ab1f
qusaiHoussien <111251532+qusaiHoussien@users.noreply.github.com>
2022-11-05 21:20:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
