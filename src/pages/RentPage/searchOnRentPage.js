9325b500c75b1a1e5679133b15645aa9362fc9df
qusaiHoussien <111251532+qusaiHoussien@users.noreply.github.com>
2022-11-13 22:44:51 +0200

    Update searchOnRentPage.js

--- src/pages/RentPage/searchOnRentPage.js
+++ src/pages/RentPage/searchOnRentPage.js
@@ -5,27 +5,27 @@ export default function search() {
   return (
     <div className="row border border-2 border-light shadow-lg  justify-content-between row-cols-md-12 ps-5 pe-5 ">
       {/**input type for location */}
-      <div className="col-3">
+      <div className="mt-2 col-12 col-md">
         <input type="text" placeholder="Location" className="form-control" />
       </div>
       {/**selects  */}
-      <FormSelect className="col-12 col-md  ">
+      <FormSelect className="mt-2 col-12 col-md  ">
         <option value="ForRent">For Rent</option>
       </FormSelect>
-      <FormSelect className="col-12 col-md ">
+      <FormSelect className="mt-2 col-12 col-md ">
         <option value="PriceRange">Price</option>
       </FormSelect>
-      <FormSelect className="col-12 col-md ">
+      <FormSelect className="mt-2 col-12 col-md ">
         <option value="Beds">Beds</option>
       </FormSelect>
-      <FormSelect className="col-12 col-md">
+      <FormSelect className="mt-2 col-12 col-md">
         <option value="MoveDate">Move Date</option>
       </FormSelect>
-      <FormSelect className="col-12 col-md">
+      <FormSelect className="mt-2 col-12 col-md">
         <option value="More">More</option>
       </FormSelect>
-      <div className="col-12 col-md"></div>
-      <Button className="col-12 col-md btn btn-sm btn-primary">
+      <div className="mt-2 col-12 col-md"></div>
+      <Button className="mt-2 col-12 col-md btn btn-sm btn-primary">
         <span className="fs-6">Search</span>
       </Button>
     </div>
